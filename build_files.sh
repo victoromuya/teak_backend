68526729f5191efaf654436bb80e3c69ab153388
unknown <viczik16@gmail.com>
2026-03-21 22:34:10 +0100

    rm local db

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 pip install -r requirements.txt
-
-pip install -r requirements.txt
 python3.9 manage.py collectstatic --no-input --clear
-python manage.py makemigrations
+
 python manage.py migrate
